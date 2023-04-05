af3d106a971a68a7a9e079abbc1af6a616c1e309
prometheus <berjistechnologies@gmail.com>
2023-04-05 04:25:17 +0300

    Add tailwind, react, react dom, react router dom

---
+++ src/components/navigation/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from "react-router";
+// Import pages here
+// Import { Page1, Page2, Page3, Page4 } from './pages';
+
+const main = () => {
+  return (
+    <Routes>
+        {/* Add pages here */}
+        {/* <Route path='/dashboard/...' element={<ComponentHere />} /> */}
+    </Routes>
+  )
+}
+
+export default main
