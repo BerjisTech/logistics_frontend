3ea02e8e7b9f3d9c05c0f0cdc85054525c333c91
prometheus <berjistechnologies@gmail.com>
2023-04-05 04:25:49 +0300

    Main component, navigation for all site

--- src/components/navigation/main.jsx
+++ src/components/navigation/main.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router";
 // Import pages here
 // Import { Page1, Page2, Page3, Page4 } from './pages';
 
-const main = () => {
+const Main = () => {
   return (
     <Routes>
         {/* Add pages here */}
@@ -12,4 +12,4 @@ const main = () => {
   )
 }
 
-export default main
+export default Main
